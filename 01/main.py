fa070b89157d76f7ee249b4b23d02880a375c538
Drewrey Lupton <drewrey@users.noreply.github.com>
2023-12-11 10:33:39 -0500

    Finish part 2 of day 1 in python

--- 01/main.py
+++ 01/main.py
@@ -1,3 +1,4 @@
+written_digits = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
 
 def read_data():
     lines = []
@@ -5,16 +6,33 @@ def read_data():
         lines = f.readlines()
     
     return lines
-def fake_data():
-    return ["1abc2", "pqr3stu8vwx", "a1b2c3d4e5f", "treb7uchet"]
+def fake_data(part = 2):
+    if part == 1:
+        data = ["1abc2", "pqr3stu8vwx", "a1b2c3d4e5f", "treb7uchet"]
+    if part ==2:
+        data = ["two1nine", "eightwothree", "abcone2threexyz", "xtwone3four", "4nineeightseven2", "zoneight234", "7pqrstsixteen"]
+    return data
 
 def find_calibration_value(line):
     first, last = 0, 0
     for i in range(len(line)):
-        if first == 0 and line[i].isdigit():
-            first = line[i]
+        if first == 0:
+            if line[i].isdigit():
+                first = line[i]
+            else:
+                for name in written_digits:
+                    if line[i:].startswith(name):
+                        first = str(written_digits.index(name) + 1)
+                        print(f"Found {name} at {i}")
+                        break
         elif line[i].isdigit():
             last = line[i]
+        else:
+            for name in written_digits:
+                if line[i:].startswith(name):
+                    last = str(written_digits.index(name) + 1)
+                    print(f"Found {name} at {i}")
+                    break
     if last == 0:
         last = first
     return int(first + last)
