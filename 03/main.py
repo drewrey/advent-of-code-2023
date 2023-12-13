7bb1b3105e22fc47449636c21984fbb175236ce0
Drewrey Lupton <drewrey@users.noreply.github.com>
2023-12-13 00:37:25 -0500

    Complete day 3 part 2 in python

--- 03/main.py
+++ 03/main.py
@@ -28,6 +28,14 @@ def find_symbol_locations(data):
                 locations.append((x, y))
     return locations
 
+def find_possible_gears(data):
+    possible_gears = []
+    for y, line in enumerate(data):
+        for x, char in enumerate(line):
+            if char == "*":
+                possible_gears.append((x, y))
+    return possible_gears
+
 def find_part_numbers(data, locations):
     part_numbers = []
     ranges_to_check = {}
@@ -45,10 +53,34 @@ def find_part_numbers(data, locations):
             
     return(part_numbers)
 
+def find_gears(data, possible_gears):
+    gear_ratios = []
+    for x, y in possible_gears:
+        x_ranges_to_check = list(set([min(max(x + x0, 0), len(data[0]) - 1) for x0 in [-1, 0, 1]]))
+        y_ranges_to_check = list(set([min(max(y + y0, 0), len(data) - 1) for y0 in [-1, 0, 1]]))
+        ranges_to_check = {}
+        for y0 in y_ranges_to_check:
+            ranges_to_check[y0] = ranges_to_check.get(y0, set()).union(x_ranges_to_check)
+
+        possible_gear_ratios = []
+
+        for y1, xs_in_y1 in ranges_to_check.items():
+            line = data[y1]
+            for match in re.finditer(r"\d+", line):
+                match_span = set(range(*match.span()))
+                if not match_span.isdisjoint(xs_in_y1):
+                    possible_gear_ratios.append(match.group())
+        if len(possible_gear_ratios) == 2:
+            gear_ratios.append(possible_gear_ratios)
+    return(gear_ratios)
 
 if __name__ == "__main__":
     # data = fake_data.split("\n")
     data = read_data()
-    symbol_locations = find_symbol_locations(data)
-    part_numbers = find_part_numbers(data, symbol_locations)
-    print(f"sum of part numbers: {sum([int(x) for x in part_numbers])}")
+    # symbol_locations = find_symbol_locations(data)
+    # part_numbers = find_part_numbers(data, symbol_locations)
+    possible_gears = find_possible_gears(data)
+    gears = find_gears(data, possible_gears)
+
+    sum_of_gear_ratios = sum(int(x) * int(y) for x, y in gears)
+    print(f"sum of gear ratios: {sum_of_gear_ratios}")
